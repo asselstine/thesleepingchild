6d60135712e142318383a08aec6647baa4a132c3
Brendan Asselstine <mail.asselstine@gmail.com>
2015-05-31 15:00:15 -0700

    Made the carousel visible on mobile, but added a darker overlay

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,4 @@
-$(document).ready(function () {
-
-  $(".m-scooch").scooch();
-  $("a.burger").click(function () {
-    $(this).closest('header, footer').toggleClass("on");
-  });
-
+function enable_scrolling_carousel() {
   window.setInterval(function () {
     $(".m-scooch").each(function () {
       if ($(this).find('.m-active').is(":last-child")) {
@@ -13,9 +7,16 @@ $(document).ready(function () {
         $(this).scooch('next');
       }
     });
-
   }, 5000);
+}
 
+$(document).ready(function () {
 
+  $(".m-scooch").scooch();
+  $("a.burger").click(function () {
+    $(this).closest('header, footer').toggleClass("on");
+  });
+
+  //enable_scrolling_carousel();
 
 });
