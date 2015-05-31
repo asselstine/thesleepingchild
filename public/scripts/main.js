6dae49e54ab152bd3580d73e053d01f6147ce035
Brendan Asselstine <mail.asselstine@gmail.com>
2015-05-31 13:50:18 -0700

    Added dotted borders before the testimonials and on the services

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,4 +5,17 @@ $(document).ready(function () {
     $(this).closest('header, footer').toggleClass("on");
   });
 
+  window.setInterval(function () {
+    $(".m-scooch").each(function () {
+      if ($(this).find('.m-active').is(":last-child")) {
+        $(this).scooch('move', 1);
+      } else {
+        $(this).scooch('next');
+      }
+    });
+
+  }, 5000);
+
+
+
 });
